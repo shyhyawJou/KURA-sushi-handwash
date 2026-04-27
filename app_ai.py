1af27eee83b3532c37284237d4a38ce8b85b9700
aaaa <fish78991@gmail.com>
2026-04-27 20:01:32 +0800

    簡易穩定版

--- app_ai.py
+++ app_ai.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 from datetime import datetime
 from pathlib import Path as p
 import re
+import numpy as np
+import socket
 from utils import (Mjpeg_Streamer, 
                    RTMDet_ONNX, 
                    Camera,
@@ -17,7 +19,9 @@ from utils import (Mjpeg_Streamer,
                    setup_logger,
                    Csv_Manager,
                    HandWashTracker,
-                   draw_timestamp)
+                   draw_timestamp,
+                   Device,
+                   Result)
 
 
 VIDEO_PATH = None
@@ -29,7 +33,7 @@ with open('utils/config.yaml') as f:
 
 
 class App_HandWash:
-    def __init__(self):
+    def __init__(self, device_code):
         self.is_running = False
 
         CFG['camera']['video_path'] = VIDEO_PATH  # 要讀取的影片
@@ -39,8 +43,10 @@ class App_HandWash:
         self.camera = Camera(**CFG['camera'])
         self.streamer = Mjpeg_Streamer(**CFG['streamer'])
         self.ai_model = RTMDet_ONNX(**CFG['AI']['handwash'])
+        self.device = Device(**CFG['device'], device_code=device_code)
         self.video = Video(**CFG['video'])
         self.csv_manager = Csv_Manager(**CFG['csv'])
+        self.result_drawer = Result(**CFG['visualization']['result'])
 
         # 檢測洗手
         self.tracker_left = HandWashTracker(zone_name="Left", logic_cfg=CFG['logic'])
@@ -67,7 +73,7 @@ class App_HandWash:
                     pbar.update(1)
 
                     if ret is None:
-                        logger.warning(f"{VIDEO_PATH} cannot read frame at {pbar.n}, skip this frame !")
+                        #logger.warning(f"{VIDEO_PATH} cannot read frame at {pbar.n}, skip this frame !")
                         continue
                     elif ret is False:
                         logger.error(f"{VIDEO_PATH} stop at {pbar.n} frame !")
@@ -76,6 +82,12 @@ class App_HandWash:
                     # AI Inference
                     with Timer("AI detect", silent=True):
                         scores, boxes, pred_labels = self.ai_model(frame)
+
+                    # 裝置 (水龍頭、水槽等物件)
+                    dev_names, dev_bboxes = self.device.named_bboxes
+                    scores = np.concatenate([scores, [1.] * len(dev_names)])
+                    boxes = np.concatenate([boxes, dev_bboxes], 0)
+                    pred_labels = np.concatenate([pred_labels, [self.ai_model.classes.index(name) for name in dev_names]])
 
                     h, w = frame.shape[:2]
                     mid_x = w // 2
@@ -87,7 +99,10 @@ class App_HandWash:
                         label = self.ai_model.classes[label_idx]
                         det = {'box': box, 'label': label, 'score': score}  # label 是類別
                         center_x = (box[0] + box[2]) / 2
-                        if center_x < mid_x:
+                        if label == 'sink':
+                            left_dets.append(det)
+                            right_dets.append(det)
+                        elif center_x < mid_x:
                             left_dets.append(det)
                         else:
                             right_dets.append(det)
@@ -104,6 +119,13 @@ class App_HandWash:
                     # visualization
                     with Timer('copy frame', silent=True):
                         frame_copy = frame.copy()
+
+                        current_steps = [f'step {self.tracker_left.current_step}, {self.tracker_left.buffer_count}', 
+                                         f'step {self.tracker_right.current_step}, {self.tracker_right.buffer_count}']
+                        self.result_drawer.draw_step(frame_copy, current_steps)
+                        #self.result_drawer.draw_region(frame_copy, np.asarray([d['box'] for d in left_dets]), 'L')
+                        #self.result_drawer.draw_region(frame_copy, np.asarray([d['box'] for d in right_dets]), 'R')
+
                         plot_bbox(frame_copy, 
                                   boxes,
                                   pred_labels, 
@@ -181,12 +203,16 @@ def get_sort_key(path_obj):
 
 
 if __name__ == "__main__":
-    paths = sorted(p('test').glob('**/*.mp4'), key=get_sort_key)
+    paths = sorted(p('video').glob('**/*.mp4'), key=get_sort_key)
     for path in paths:
+        if path.name != '20260410_1.mp4':
+            continue
+
         try:
             VIDEO_PATH = path
-            setup_logger(**CFG['log'], suffix=socket.gethostname().split('-')[-1])
-            app = App_HandWash()
+            device_code = socket.gethostname().split('-')[-1]
+            setup_logger(**CFG['log'], suffix=device_code)
+            app = App_HandWash(device_code)
             app.run()
         except SystemExit:
             logger.success('Application terminated.')
