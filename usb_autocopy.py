65b6d167943cb7d9432333f4459c2ed68312769d
aaaa <fish78991@gmail.com>
2026-07-02 12:49:03 +0800

    fsync 改成 os.sync

--- usb_autocopy.py
+++ usb_autocopy.py
@@ -16,7 +16,7 @@ import glob
 SOURCE_DIR = "/mnt/reserved/record/stream"   # 影片來源資料夾
 DEFAULT_MOUNT = "/mnt/usb"                   # 預設指定的掛載點
 CURRENT_MOUNT = ''
-CHECK_INTERVAL = 1800                          # 檢查間隔（秒）
+CHECK_INTERVAL = 1800                     # 檢查間隔（秒）
 LOG_PATH = "/mnt/reserved/usb_autocopy.log"  # LOG 路徑
 DELETE_OLD = True
 DELETE_FAILED = True
@@ -234,18 +234,24 @@ def sync_files():
         all_success = True
         paths = list(mark_path.parent.glob(f'{mark_path.stem}.*'))
         
+        copied_files = []
         for path in paths:
             try:
                 logger.info(f"Copying {path} to USB...")
                 dst = f'{CURRENT_MOUNT}/{"/".join(path.parts[-2:])}'
                 os.makedirs(dirname(dst), exist_ok=True)
                 shutil.copy2(path, dst) # copy2 會保留元數據
+                copied_files.append([path, dst])
 
-                # 強制將快取寫入磁碟
-                with open(dst, 'rb') as f:
-                    os.fsync(f.fileno())       
+            except Exception as e:
+                logger.error(f"Error copying {path}: {e}")
 
-                # 驗證 MD5 確保檔案正確
+        # sync
+        os.sync()
+
+        # 驗證 MD5 確保檔案正確
+        for path, dst in copied_files:
+            try:
                 if get_md5(path) == get_md5(dst):
                     logger.success(f"Verification Success: {dst}")                    
                 else:
